8b301d48fdb454493f78d3482b9115bc5b97d57a
Felipe <felipe.carvalho.dm@gmail.com>
2025-09-09 00:34:41 -0300

    iniciando projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus{
+        outline: 0;
+        box-shadow: 0 0 0 2px ${props => props.theme['yellow-dark']};
+    }
+
+    body{
+        background-color: ${props => props.theme['background']};
+        color: ${props => props.theme['base-title']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    h1, h2, h3{
+        font-family: "Baloo 2", sans-serif;
+        font-weight: 900;
+        line-height: 130%;
+    }
+
+    h4, h5{
+        font-family: "Baloo 2", sans-serif;
+        font-weight: 800;
+        line-height: 130%;
+    }
+
+    body, input, text-area, button{
+        font-family: "Roboto", sans-serif;
+        line-height: 130%;
+    }
+`;
